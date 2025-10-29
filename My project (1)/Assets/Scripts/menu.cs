0cf97e6921673fa67cf56412ed411e63459244b1
tanisthegoat11 <tanner.c729@stu.nebo.edu>
2025-10-29 14:15:45 -0600

    sigma

---
+++ My project (1)/Assets/Scripts/menu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+public class menu : MonoBehaviour
+{
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OnQuitButton()
+    {
+        Application.Quit(); // Quit Game
+    }
+}
